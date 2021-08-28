57232e792de96dc4f3c4f7af8f66cebcd7f1a57b
ZunairaMaqbool <zunairamaqbool007@gmail.com>
2021-08-28 20:41:33 +0500

    Madlibs game completed

---
+++ main.py
@@ -0,0 +1,5 @@
+Noun = input("Please Enter a Noun here: ")
+Verb = input("Please Enter a Verb here: ")
+Celebrity_name = input("Please Enter a Celebrity Name here: ")
+Paragraph = "Today i was going for a " + Verb + " but i encountered a " + Noun + " So i stayed home and watched the movies of " + Celebrity_name
+print(Paragraph)
